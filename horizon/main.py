95d9d485e51587ccd6a55a87712ceb5f6cfc498c
Asaf Cohen <asafc@me.com>
2021-03-29 12:38:08 +0300

    remove policy updater (now implemented by opal client)

--- horizon/main.py
+++ horizon/main.py
@@ -1,4 +1,3 @@
-import asyncio
 from horizon.config import OPENAPI_TAGS_METADATA
 from fastapi import FastAPI
 
@@ -7,10 +6,8 @@ logging_config.set_mode(LoggingModes.UVICORN)
 
 from horizon.server.api import router as proxy_router
 from horizon.enforcer.api import router as enforcer_router
-from horizon.policy.api import router as policy_router
 from horizon.local.api import router as local_router
 from horizon.server.middleware import configure_middleware
-from horizon.policy.updater import policy_updater
 from horizon.enforcer.runner import opa_runner
 
 app = FastAPI(
@@ -26,21 +23,9 @@ configure_middleware(app)
 # include the api routes
 app.include_router(enforcer_router, tags=["Authorization API"])
 app.include_router(local_router, prefix="/local", tags=["Local Queries"])
-app.include_router(policy_router, tags=["Policy Updater"])
 app.include_router(proxy_router, tags=["Cloud API Proxy"])
 
 @app.get("/healthcheck", include_in_schema=False)
 @app.get("/", include_in_schema=False)
 def healthcheck():
     return {"status": "ok"}
-
-@app.on_event("startup")
-async def startup_event():
-    opa_runner.start()
-    await asyncio.sleep(1) # wait for opa
-    policy_updater.start()
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await policy_updater.stop()
-    opa_runner.stop()
