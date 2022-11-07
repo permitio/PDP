51a6a61a337991e3ec09934246a31ae16505306d
Shaul Kremer <shaul@permit.io>
2022-11-07 16:19:23 +0200

    Updated start.sh to use Debian's bash properly.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,10 @@
-#! /usr/bin/env sh
+#!/bin/bash
 set -e
 
 export GUNICORN_CONF=${GUNICORN_CONF:-/gunicorn_conf.py}
-
-# Start Gunicorn
-if [[ -z "${PDP_ENABLE_MONITORING}" && "${PDP_ENABLE_MONITORING}" = "true" ]]; then
-  exec ddtrace-run gunicorn -b 0.0.0.0:${UVICORN_PORT} -k uvicorn.workers.UvicornWorker --workers=${UVICORN_NUM_WORKERS} -c ${GUNICORN_CONF} ${UVICORN_ASGI_APP}
-else
-  exec gunicorn -b 0.0.0.0:${UVICORN_PORT} -k uvicorn.workers.UvicornWorker --workers=${UVICORN_NUM_WORKERS} -c ${GUNICORN_CONF} ${UVICORN_ASGI_APP}
+ddtrace=""
+if [ "${PDP_ENABLE_MONITORING}" == "true" ]
+then
+    ddtrace=ddtrace-run
 fi
+exec $ddtrace gunicorn -b 0.0.0.0:${UVICORN_PORT} -k uvicorn.workers.UvicornWorker --workers=${UVICORN_NUM_WORKERS} -c ${GUNICORN_CONF} ${UVICORN_ASGI_APP}
