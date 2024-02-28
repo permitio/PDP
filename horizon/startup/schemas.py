6e2bd4263180462957a2bb9de17da6945536496d
elimoshkovich <tbhjfo14@gmail.com>
2024-02-28 17:09:06 +0200

    Initial Commit

--- horizon/startup/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class RemoteConfig(BaseModel):
-    opal_common: dict = {}
-    opal_client: dict = {}
-    pdp: dict = {}
-    context: dict = {}
--- horizon/system/schemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class VersionResult(BaseModel):
-    api_version: int
