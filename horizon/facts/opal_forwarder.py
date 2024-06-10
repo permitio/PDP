6966f373713f65ff0ad1c9d2f727aebede52b1df
Dan Yishai <danyi1212@users.noreply.github.com>
2024-06-10 13:22:34 +0300

    Moved generate opal data source entry to module

---
+++ horizon/facts/opal_forwarder.py
@@ -0,0 +1,61 @@
+from functools import cache
+from urllib.parse import urljoin
+
+from opal_common.fetcher.providers.http_fetch_provider import HttpFetcherConfig
+from opal_common.schemas.data import DataSourceEntry
+
+from config import sidecar_config
+from startup.remote_config import get_remote_config
+
+
+@cache
+def get_opal_data_base_url() -> str:
+    remote_config = get_remote_config()
+    org_id = remote_config.context.get("org_id")
+    proj_id = remote_config.context.get("project_id")
+    env_id = remote_config.context.get("env_id")
+    return urljoin(
+        sidecar_config.CONTROL_PLANE,
+        f"/v2/internal/opal_data/{org_id}/{proj_id}/{env_id}",
+    )
+
+
+@cache
+def get_opal_data_topic() -> str:
+    remote_config = get_remote_config()
+    pdp_client_id = remote_config.context.get("client_id")
+    topic = f"{pdp_client_id}:data:policy_data/{pdp_client_id}"
+    if sidecar_config.SHARD_ID:
+        topic += f"?shard_id={sidecar_config.SHARD_ID}"
+
+    return topic
+
+
+def generate_opal_data_source_entry(
+    obj_type: str,
+    obj_id: str,
+    obj_key: str,
+    authorization_header: str,
+) -> DataSourceEntry:
+    obj_id = obj_id.replace("-", "")  # convert UUID to Hex
+    url = urljoin(
+        get_opal_data_base_url(),
+        f"/{obj_type}/{obj_id}",
+    )
+
+    topic = get_opal_data_topic()
+
+    headers = {
+        "Authorization": authorization_header,
+    }
+    if sidecar_config.SHARD_ID:
+        headers["X-Shard-Id"] = sidecar_config.SHARD_ID
+
+    return DataSourceEntry(
+        url=url,
+        data=None,
+        dst_path=f"{obj_type}/{obj_key}",
+        save_method="PUT",
+        topics=[topic],
+        config=HttpFetcherConfig(headers=headers).dict(),
+    )
--- horizon/facts/router.py
+++ horizon/facts/router.py
@@ -1,72 +1,14 @@
-from functools import cache
 from typing import Annotated
-from urllib.parse import urljoin
 
 from fastapi import APIRouter, Depends, Request as FastApiRequest
 from loguru import logger
 from opal_client import OpalClient
-from opal_common.fetcher.providers.http_fetch_provider import HttpFetcherConfig
-from opal_common.schemas.data import DataSourceEntry
 
 from authentication import enforce_pdp_token
-from config import sidecar_config
 from facts.client import FactsClientDependency
-from startup.remote_config import get_remote_config
+from facts.opal_forwarder import generate_opal_data_source_entry
 
 facts_router = APIRouter(dependencies=[Depends(enforce_pdp_token)])
-
-
-@cache
-def get_opal_data_base_url() -> str:
-    remote_config = get_remote_config()
-    org_id = remote_config.context.get("org_id")
-    proj_id = remote_config.context.get("project_id")
-    env_id = remote_config.context.get("env_id")
-    return urljoin(
-        sidecar_config.CONTROL_PLANE,
-        f"/v2/internal/opal_data/{org_id}/{proj_id}/{env_id}",
-    )
-
-
-@cache
-def get_opal_data_topic() -> str:
-    remote_config = get_remote_config()
-    pdp_client_id = remote_config.context.get("client_id")
-    topic = f"{pdp_client_id}:data:policy_data/{pdp_client_id}"
-    if sidecar_config.SHARD_ID:
-        topic += f"?shard_id={sidecar_config.SHARD_ID}"
-
-    return topic
-
-
-def generate_opal_data_source_entry(
-    obj_type: str,
-    obj_id: str,
-    obj_key: str,
-    authorization_header: str,
-) -> DataSourceEntry:
-    obj_id = obj_id.replace("-", "")  # convert UUID to Hex
-    url = urljoin(
-        get_opal_data_base_url(),
-        f"/{obj_type}/{obj_id}",
-    )
-
-    topic = get_opal_data_topic()
-
-    headers = {
-        "Authorization": authorization_header,
-    }
-    if sidecar_config.SHARD_ID:
-        headers["X-Shard-Id"] = sidecar_config.SHARD_ID
-
-    return DataSourceEntry(
-        url=url,
-        data=None,
-        dst_path=f"{obj_type}/{obj_key}",
-        save_method="PUT",
-        topics=[topic],
-        config=HttpFetcherConfig(headers=headers).dict(),
-    )
 
 
 @facts_router.post("/users")
