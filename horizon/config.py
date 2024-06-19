0214e552261696d7dae404ae07f932bc068faea0
Omer Zuarets <omer@permit.io>
2024-06-19 15:01:28 +0300

    add description for LOCAL_FACTS_WAIT_TIMEOUT config

--- horizon/config.py
+++ horizon/config.py
@@ -170,7 +170,10 @@ class SidecarConfig(Confi):
     # enables debug ouptut for the Kong integration endpoint
     KONG_INTEGRATION_DEBUG = confi.bool("KONG_INTEGRATION_DEBUG", False)
 
-    LOCAL_FACTS_WAIT_TIMEOUT = confi.float("LOCAL_FACTS_WAIT_TIMEOUT", 10)
+    LOCAL_FACTS_WAIT_TIMEOUT = confi.float(
+        "LOCAL_FACTS_WAIT_TIMEOUT", 10,
+        description="The amount of time in seconds to wait for the local facts to be synced before timing out"
+    )
 
     # non configurable values -------------------------------------------------
 
@@ -179,13 +182,13 @@ class SidecarConfig(Confi):
         {
             "name": "Authorization API",
             "description": "Authorization queries to OPA. These queries are answered locally by OPA "
-            + "and do not require the cloud service. Latency should be very low (< 20ms per query)",
+                           + "and do not require the cloud service. Latency should be very low (< 20ms per query)",
         },
         {
             "name": "Local Queries",
             "description": "These queries are done locally against the sidecar and do not "
-            + "involve a network round-trip to Permit.io cloud API. Therefore they are safe "
-            + "to use with reasonable performance (i.e: with negligible latency) in the context of a user request.",
+                           + "involve a network round-trip to Permit.io cloud API. Therefore they are safe "
+                           + "to use with reasonable performance (i.e: with negligible latency) in the context of a user request.",
         },
         {
             "name": "Policy Updater",
@@ -194,10 +197,10 @@ class SidecarConfig(Confi):
         {
             "name": "Cloud API Proxy",
             "description": "These endpoints proxy the Permit.io cloud api, and therefore **incur high-latency**. "
-            + "You should not use the cloud API in the standard request flow of users, i.e in places where the incurred "
-            + "added latency will affect your entire api. A good place to call the cloud API will be in one-time user events "
-            + "such as user registration (i.e: calling sync user, assigning initial user roles, etc.). "
-            + "The sidecar will proxy to the cloud every request prefixed with '/sdk'.",
+                           + "You should not use the cloud API in the standard request flow of users, i.e in places where the incurred "
+                           + "added latency will affect your entire api. A good place to call the cloud API will be in one-time user events "
+                           + "such as user registration (i.e: calling sync user, assigning initial user roles, etc.). "
+                           + "The sidecar will proxy to the cloud every request prefixed with '/sdk'.",
             "externalDocs": {
                 "description": "The cloud api complete docs are located here:",
                 "url": "https://api.permit.io/redoc",
