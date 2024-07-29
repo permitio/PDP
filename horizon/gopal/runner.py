960d384a70f859202d497d5d1935331601552b56
Omer Zuarets <omer@permit.io>
2024-07-29 16:01:25 +0300

    add healthy and ready checks to GOPALClient

--- horizon/gopal/runner.py
+++ horizon/gopal/runner.py
@@ -2,17 +2,63 @@ import os
 import platform
 from pathlib import Path
 
+import aiohttp
+from opal_client.config import EngineLogFormat
 from opal_client.engine.runner import PolicyEngineRunner
 
 from horizon.config import sidecar_config
 
 
 class GopalRunner(PolicyEngineRunner):
+    def __init__(
+        self,
+        engine_token: str,
+        engine_url: str,
+        piped_logs_format: EngineLogFormat = EngineLogFormat.NONE,
+    ):
+        super().__init__(piped_logs_format=piped_logs_format)
+        self._engine_token = engine_token
+        self._engine_url = engine_url
+        self.__client = None
+
+    def _client(self) -> aiohttp.ClientSession:
+        if self.__client is None:
+            self.__client = aiohttp.ClientSession(base_url=self._engine_url,headers={
+                "Authorization": f"Bearer {self._engine_token}"
+            })
+        return self.__client
+
+    async def __aenter__(self):
+        await super().__aenter__()
+        await self._client().__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await super().__aexit__(exc_type, exc_val, exc_tb)
+        await self._client().__aexit__(exc_type, exc_val, exc_tb)
+
+    async def is_healthy(self) -> bool:
+        async with self._client().get("/healthy") as resp:
+            try:
+                resp.raise_for_status()
+            except aiohttp.ClientResponseError:
+                return False
+            else:
+                return True
+
+    async def is_ready(self) -> bool:
+        async with self._client().get("/ready") as resp:
+            try:
+                resp.raise_for_status()
+            except aiohttp.ClientResponseError:
+                return False
+            else:
+                return True
 
     @property
     def command(self) -> str:
         current_dir = Path(__file__).parent
-        os.environ["PDP_ENGINE_TOKEN"] = sidecar_config.API_KEY
+        os.environ["PDP_ENGINE_TOKEN"] = self._engine_token
         arch = platform.machine()
         if arch == 'x86_64':
             binary_path = 'gopal-amd'
