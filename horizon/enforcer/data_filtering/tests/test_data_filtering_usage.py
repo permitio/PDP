2156b4bce6900968925a214260dcdfe8a8daf5fb
Asaf Cohen <asaf@permit.io>
2024-08-08 16:34:20 +0300

    more tests

--- horizon/enforcer/data_filtering/tests/test_data_filtering_usage.py
+++ horizon/enforcer/data_filtering/tests/test_data_filtering_usage.py
@@ -1,3 +1,5 @@
+import pytest
+
 from horizon.enforcer.data_filtering.boolean_expression.schemas import (
     ResidualPolicyResponse,
 )
@@ -142,4 +144,124 @@ def test_sql_translation_with_join():
         """SELECT task_joined.id, task_joined.created_at, task_joined.updated_at, task_joined.description, task_joined.tenant_id_joined 
         FROM task_joined JOIN tenant ON task_joined.tenant_id_joined = tenant.id 
         WHERE tenant.key = '082f6978-6424-4e05-a706-1ab6f26c3768'"""
+    )
+
+
+def test_sql_translation_of_trivial_policies():
+    class Tasks(Base):
+        __tablename__ = "tasks"
+
+        id = Column(String, primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow())
+        updated_at = Column(DateTime)
+        description = Column(String(255))
+        tenant_id = Column(String(255))
+
+    filter = ResidualPolicyResponse(**{"type": "always_allow", "condition": None})
+
+    sa_query = to_query(
+        filter,
+        Tasks,
+        refs={
+            # example how to map a column on the same model
+            "input.resource.tenant": Tasks.tenant_id,
+        },
+    )
+
+    str_query = query_to_string(sa_query)
+    assert striplines(str_query) == striplines(
+        """SELECT tasks.id, tasks.created_at, tasks.updated_at, tasks.description, tasks.tenant_id 
+        FROM tasks"""
+    )  # this query would always return all rows from the tasks table
+
+    filter = ResidualPolicyResponse(**{"type": "always_deny", "condition": None})
+
+    sa_query = to_query(
+        filter,
+        Tasks,
+        refs={
+            # example how to map a column on the same model
+            "input.resource.tenant": Tasks.tenant_id,
+        },
+    )
+
+    str_query = query_to_string(sa_query)
+    assert striplines(str_query) == striplines(
+        """SELECT tasks.id, tasks.created_at, tasks.updated_at, tasks.description, tasks.tenant_id 
+        FROM tasks
+        WHERE false"""
+    )  # this query would never have any results
+
+
+def test_missing_joins():
+    filter = ResidualPolicyResponse(
+        **{
+            "type": "conditional",
+            "condition": {
+                "expression": {
+                    "operator": "eq",
+                    "operands": [
+                        {"variable": "input.resource.tenant"},
+                        {"value": "082f6978-6424-4e05-a706-1ab6f26c3768"},
+                    ],
+                }
+            },
+        }
+    )
+
+    class User2(Base):
+        __tablename__ = "user2"
+
+        id = Column(String, primary_key=True)
+        username = Column(String(255))
+
+    class Tenant2(Base):
+        __tablename__ = "tenant2"
+
+        id = Column(String, primary_key=True)
+        key = Column(String(255))
+
+    class TaskJoined2(Base):
+        __tablename__ = "task_joined2"
+
+        id = Column(String, primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow())
+        updated_at = Column(DateTime)
+        description = Column(String(255))
+        tenant_id_joined = Column(String, ForeignKey("tenant2.id"))
+        tenant = relationship("Tenant2", backref="tasks")
+        owner_id = Column(String, ForeignKey("user2.id"))
+        owner = relationship("User2", backref="tasks")
+
+    with pytest.raises(TypeError) as e:
+        # Tenant2.key is a column outside the main table (requires a join)
+        # if we don't provide any join conditions, to_query() will throw a TypeError
+        sa_query = to_query(
+            filter,
+            TaskJoined2,
+            refs={
+                # example how to map a column on a related model
+                "input.resource.tenant": Tenant2.key,
+            },
+        )
+
+    assert str(e.value) == "to_query() is missing argument 'join_conditions'"
+
+    with pytest.raises(TypeError) as e:
+        # Tenant2.key is a column outside the main table (requires a join)
+        # if we provide join conditions but not to all required tables,
+        # to_query() will throw a different TypeError
+        sa_query = to_query(
+            filter,
+            TaskJoined2,
+            refs={
+                # example how to map a column on a related model
+                "input.resource.tenant": Tenant2.key,
+            },
+            join_conditions=[(User2, TaskJoined2.owner_id == User2.id)],
+        )
+
+    assert (
+        str(e.value)
+        == "to_query() argument 'join_conditions' is missing mapping for tables: {'tenant2'}"
     )
